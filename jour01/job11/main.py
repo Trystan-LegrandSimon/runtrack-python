86a064a44e9f7e5e7b0a020f8303511eea23b221
DevHack136 <trystan.legrand-simon@epitech.eu>
2023-11-20 14:02:51 +0100

    update

---
+++ jour01/job11/main.py
@@ -0,0 +1,8 @@
+# Demander à l'utilisateur de saisir une chaîne
+chaine_utilisateur = input("Entrez une chaîne de caractères : ")
+
+# Vérifier si la chaîne contient le caractère 'e'
+if 'e' in chaine_utilisateur:
+    print("La chaîne contient le caractère 'e'.")
+else:
+    print("La chaîne ne contient pas le caractère 'e'.")
