00bc8e693453ce2a247ac8b325c5455d4d09015d
__DevHack__ <trystan.legrand-simon@epitech.eu>
2023-11-22 11:23:44 +0100

    update day03 and job03

---
+++ jour03/job03/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def Add(number1, number2) -> int:
+    result = number1 + number2
+    print(f"The sum of {number1} and {number2} is equal to {result}.")
+
+Add(10, 2)
+Add(15, 20)
+Add(5, 25)
+Add(12, 22)
