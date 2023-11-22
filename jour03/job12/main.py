210d427dbe321d3c5991ed7c9a207dcab39c9df7
__DevHack__ <trystan.legrand-simon@epitech.eu>
2023-11-22 12:56:25 +0100

    update day03 and job012

---
+++ jour03/job12/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def inverser_string(chaine):
+    return chaine[::-1]
+
+chaine_originale = "nikana"
+chaine_inversee = inverser_string(chaine_originale)
+
+print(f"Chaine originale : {chaine_originale}")
+print(f"Chaine inversée : {chaine_inversee}")
